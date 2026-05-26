097a08eb24c9d3475de89a64d9b50e23ff1a2ec8
AyazKemal <kemayaz@gmail.com>
2026-05-26 06:44:27 +0300

    End of Section 4

--- API/Program.cs
+++ API/Program.cs
@@ -15,6 +15,7 @@ builder.Services.AddDbContext<StoreContext>(opt =>
 //builder.Services.AddOpenApi();
 
 builder.Services.AddScoped<IProductRepository, ProductRepository>();
+builder.Services.AddScoped(typeof(IGenericRepository<>), typeof(GenericRepository<>));
 
 var app = builder.Build();
 
