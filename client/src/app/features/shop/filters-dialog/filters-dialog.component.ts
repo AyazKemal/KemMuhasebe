1e82ad8c6d975b5d49a7254f3050abe87004f3cc
AyazKemal <kemayaz@gmail.com>
2026-05-28 16:03:56 +0300

    End og Section 9

---
+++ client/src/app/features/shop/filters-dialog/filters-dialog.component.ts
@@ -0,0 +1,34 @@
+import { Component, inject } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { MatButton } from '@angular/material/button';
+import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { MatDivider, MatListOption, MatSelectionList } from '@angular/material/list';
+import { ShopService } from '../../../core/services/shop.service';
+
+@Component({
+  selector: 'app-filters-dialog',
+  imports: [
+     MatDivider,
+    MatSelectionList,
+    MatListOption,
+    MatButton,
+    FormsModule
+  ],
+  templateUrl: './filters-dialog.component.html',
+  styleUrl: './filters-dialog.component.css',
+})
+export class FiltersDialogComponent {
+    shopService = inject(ShopService);
+  private dialogRef = inject(MatDialogRef<FiltersDialogComponent>);
+  data = inject(MAT_DIALOG_DATA);
+
+  selectedBrands: string[] = this.data.selectedBrands;
+  selectedTypes: string[] = this.data.selectedTypes;
+
+  applyFilters() {
+    this.dialogRef.close({
+      selectedBrands: this.selectedBrands,
+      selectedTypes: this.selectedTypes
+    })
+  }
+}
